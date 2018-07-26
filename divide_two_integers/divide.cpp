7b442a5353225cd27449b322be219441bc71d9d0
Hsiangkai <Hsiangkai@gmail.com>
2018-07-26 16:45:56 +0000

    Take care overflow.

--- divide_two_integers/divide.cpp
+++ divide_two_integers/divide.cpp
@@ -2,29 +2,85 @@
 
 using namespace std;
 
+#define MAX_INT 2147483647
+
 class Solution {
 public:
   int divide(int dividend, int divisor) {
     bool minus = false;
+    bool overflow = false;
     if (dividend < 0 && divisor > 0) {
-      dividend = -dividend;
       minus = true;
     }
     if (dividend > 0 && divisor < 0) {
-      divisor = -divisor;
       minus = true;
     }
-    if (dividend < 0 && divisor < 0) {
-      dividend = -dividend;
-      divisor = -divisor;
-    }
+
+    if (divisor == 1)
+      return dividend;
+    if (divisor == 2)
+      return dividend >> 1;
 
     int quotient = 0;
     int remain = dividend;
-    while (remain >= divisor) {
-      quotient++;
-      remain -= divisor;
+    if (dividend > 0 && divisor > 0) {
+      while (remain >= divisor) {
+	quotient++;
+	remain -= divisor;
+      }
+    } else if (dividend < 0 && divisor < 0) {
+      while (remain <= divisor) {
+	if (quotient == MAX_INT) {
+	  overflow = true;
+	  break;
+	}
+	quotient++;
+	remain -= divisor;
+      }
+    } else if (dividend > 0 && divisor < 0) {
+      if (divisor != -divisor) {
+	while (remain >= -divisor) {
+	  if (quotient == MAX_INT) {
+	    overflow = true;
+	    break;
+	  }
+	  quotient++;
+	  remain += divisor;
+	}
+      } else {
+	while (-remain <= divisor) {
+	  if (quotient == MAX_INT) {
+	    overflow = true;
+	    break;
+	  }
+	  quotient++;
+	  remain += divisor;
+	}
+      }
+    } else if (dividend < 0 && divisor > 0) {
+      if (divisor != -divisor) {
+	while (remain <= -divisor) {
+	  if (quotient == MAX_INT) {
+	    overflow = true;
+	    break;
+	  }
+	  quotient++;
+	  remain += divisor;
+	}
+      } else {
+	while (-remain >= divisor) {
+	  if (quotient == MAX_INT) {
+	    overflow = true;
+	    break;
+	  }
+	  quotient++;
+	  remain += divisor;
+	}
+      }
     }
+
+    if (overflow)
+      return MAX_INT;
 
     if (minus)
       quotient = -quotient;
@@ -41,4 +97,10 @@ int main()
   cout << "7 / 3 = " << sol.divide(7, 3) << endl;
   cout << "-7 / -3 = " << sol.divide(-7, -3) << endl;
   cout << "1 / 1 = " << sol.divide(1, 1) << endl;
+  cout << "2147483647 / 1 = " << sol.divide(2147483647, 1) << endl;
+  cout << "-2147483648 / -1 = " << sol.divide(-2147483648, -1) << endl;
+  cout << "-2147483648 / -3 = " << sol.divide(-2147483648, -3) << endl;
+  cout << "-2147483648 / 3 = " << sol.divide(-2147483648, 3) << endl;
+  cout << "1038925803 / -2147483648 = " << sol.divide(1038925803, -2147483648) << endl;
+  cout << "3 / -7 = " << 3 / -7 << endl;
 }
